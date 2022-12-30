179e115e8c15d5ae8f786b3da271f8e5d7cc21e5
Ayou Lasme <ayou.lasme@mail.com>
2022-12-30 13:56:33 +0000

    Mini prjet ExerciceXp jour5 Week2

---
+++ script.js
@@ -0,0 +1,42 @@
+// partie1
+
+function playTheGame() {
+    let confirmation = confirm("Souhaitez vous jouer le jeu?");
+    if (confirmation == false) {
+        alert("Pas de probleme aurevoir");
+    }else{
+        let numero = prompt("Entrer un nombre entre 0 et 10");
+        typeof(numero);
+        if (isNaN(numero)) {
+            alert("Désolé, pas un numéro, au revoir ");
+        }else {
+            if(numero >= 0 && numero < 10){
+                let computerNumber = Math.floor(Math.random() * 10);
+                console.log(computerNumber);
+            }else{
+                alert("Désolé, ce n'est pas un bon chiffre, au revoir");
+            } 
+        }
+    }
+  }
+
+
+  // partie2
+
+function compareNumbers(userNumber, computerNumber) {
+    do {
+        const nombreDeChance = 3;
+        let i = 0;
+        if(userNumber == computerNumber){
+            alert("WINNER");
+            return;
+        }else if (userNumber > computerNumber){
+            alert("Votre numéro est plus grand que celui de l'ordinateur, devinez à nouveau");
+            userNumber = prompt("Entrer un nouveau numero")
+        }else {
+            alert("Votre numéro est plus petit que celui de l'ordinateur, devinez à nouveau");
+            userNumber = prompt("Entrer un nouveau numero")
+        }
+    } while (i < nombreDeChance);
+}
+
